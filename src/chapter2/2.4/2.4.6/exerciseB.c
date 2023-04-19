01394f5cfddd04e47cf9ff9aa43a6b73a9334263
artjoms.voroncovs <artv20022@gmail.com>
2023-04-19 17:29:07 +0300

    Exercise 2.4.6 completed!

--- src/chapter2/2.4/2.4.6/exerciseB.c
+++ src/chapter2/2.4/2.4.6/exerciseB.c
@@ -1,2 +1,56 @@
-# S -> S ( S ) S | e
+#include <stdio.h>
+#include <stdlib.h>
+// S -> S ( S ) S | ε
+
+char input[100];
+char *input_end = input;
+char *current = input;
+
+void S();
+int match(char c);
+
+int main()
+{
+    printf("Input string: \n");
+    scanf("%s", input);
+    
+    
+    while(*input_end++);
+    input_end--;
+    
+    S();
+    
+    if(input_end != current) {
+        printf("Syntax error in length\n");
+        exit(1);
+    } 
+    printf("Syntax is correct\n");
+
+    return 0;
+}
+
+void S(){
+    switch(*current){
+        case '(':
+            match('(');
+            S();
+            match(')');
+            S();
+            break;
+        
+        default:
+           return;
+    }
+}
+
+int match(char c){
+    if(c == *current) {
+        current++;
+    } else {
+        printf("Syntax error in match\n");
+        exit(1);
+    }
+    
+    return 0;
+}
 
